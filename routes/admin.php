3b16b9e90c557e6a69860fa9ba765d0f42b86834
nadia1624 <nadyadearihanifah@gmail.com>
2025-06-26 00:48:14 +0700

    dashboard admin

--- routes/admin.php
+++ routes/admin.php
@@ -4,12 +4,11 @@ use App\Http\Controllers\Admin\MahasiswaController;
 use App\Http\Controllers\Admin\DosenController;
 use App\Http\Controllers\Admin\PengajuanController;
 use App\Http\Controllers\Admin\DetailPengajuanController;
+use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
+    Route::get('/admin/dashboard', [MahasiswaController::class, 'dashboard'])->name('admin.dashboard');
 
     Route::get('/admin/mahasiswa', [MahasiswaController::class, 'index'])->name('admin.mahasiswa');
 
